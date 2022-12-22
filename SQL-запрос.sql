8b2b937125837a1214d5da90eb5e26b84dff7fd5
Nameless-Mdr <77780010+Nameless-Mdr@users.noreply.github.com>
2022-12-22 20:20:53 +0300

    Update SQL-запрос.sql

--- SQL-запрос.sql
+++ SQL-запрос.sql
@@ -1,5 +1,4 @@
-select p.name, c.name
-from products p 
-left join prod_categ pc on p.id = pc.product_id
-left join categories c on pc.category_id = c.id
-order by p.name
+select p.name product, COALESCE(c.name, ' ') categorie
+from products p left join pr_ct pc on p.id = pc.pr_id
+left join categories c on pc.ct_id = c.id
+order by p.id;
