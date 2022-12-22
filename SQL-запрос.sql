11c9a8170116c524fdedfd39e9de410c277544f2
Nameless-Mdr <77780010+Nameless-Mdr@users.noreply.github.com>
2022-12-22 19:58:10 +0300

    Update SQL-запрос.sql

--- SQL-запрос.sql
+++ SQL-запрос.sql
@@ -1,6 +1,5 @@
 select p.name, c.name
 from products p 
 left join prod_categ pc on p.id = pc.product_id
-left join categories c on pc.category_id = c.id 
-group by p.name, c.name
+left join categories c on pc.category_id = c.id
 order by p.name
